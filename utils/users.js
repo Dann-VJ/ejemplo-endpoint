27b3d4da7244df871b8b5ff115f489a4c5508591
Dann-VJ <danielvazquezjoaquin236@gmail.com>
2023-06-22 23:50:36 -0600

    first commit

---
+++ utils/users.js
@@ -0,0 +1,24 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+//Varibles de entorno con env
+const users = [
+  {
+    id: process.env.USERNAME,
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    fullname: process.env.FULLNAME,
+  },
+];
+
+console.log(users);
+//Validación
+export const getUser = (username, password) => {
+  const user = users.find((user) => user.username === username);
+
+  if (!user || user.password !== password) {
+    throw new Error("Credenciales invalidas");
+  }
+
+  return user;
+};
